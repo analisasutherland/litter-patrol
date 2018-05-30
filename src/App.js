24b472c8aab4654c8e995803e2920c01551f3431
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2018-05-30 15:47:21 -0700

    Finished and able to increment points upon trash image click

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import './App.css';
 import Trash from './components/Trash.js';
 
 class App extends Component {
+  // when component starts up it sets the initial state of the bins by calling the getBinsState fxn and sets initial points to 0
   constructor() {
     super();
     this.state = {
       bins: this.getBinsState(),
+      // this checks if the trash is visible and where it shows up by calling getBinsState fxn
       points: 0
     };
 
     this.startGame();
+    // the constructor also calls the fxn startGame, this will set the interval for when the bins move.
     console.log(this.state.bins);
   }
 
+  // it does this by calling setState with will overwrite bins with a new call on getBinsState. This will then render the componenet again and get display a new randomized layout of the the trash bins w/without trash in them.
   startGame() {
     setInterval(() => {
       this.setState( {
@@ -22,23 +26,38 @@ class App extends Component {
     }, 1500);
   }
 
+  // getBinsState gives us an array of bins
+  // overall is checking if visible trash will be displayed in a given bin
   getBinsState() {
     let bins = [];
     for (let i = 0; i < 9; i++){
-      bins.push({ isTrashVisible: (Math.round(Math.random()) ? true : false )});
+      bins.push({ isTrashVisible:
+        // is pushing the given bins where trash is into bins array
+      (Math.round(Math.random()) ? true : false )});
+      // so returning a random num between 0-1, rounding that num, and evaluating if it is truthy or falsey. 0 is fale in JS
+      // Math.random() > 0.5
     }
 
     return bins;
   }
 
+  // this will allow us to click on trash as it appears randomly in the defined bins. On click event for the trash a point will be given to the user
   onTrashClicked = () => {
-    // Fill this in!
+    console.log(this);
+    this.setState( {
+      points: this.state.points + 1
+    });
   }
 
   render() {
+    // mapping over the bins array and displaying the visible trash
     const bins = this.state.bins.map((bin, index) => {
       return (
-        <Trash key={`trash-${index}`} />
+        // whatever index the trash was at is assigned as the unique trash key to identify them from each other
+        <Trash key={`trash-${index}`}
+        isTrashVisible={bin.isTrashVisible}
+        trashCallback={this.onTrashClicked}
+        />
       );
     });
 
--- src/components/Trash.js
+++ src/components/Trash.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 import '../App.css';
+import PropTypes from 'prop-types';
 import TrashIcon from '../trash.svg';
 
 class Trash extends Component {
+  static propTypes = {
+    isTrashVisible: PropTypes.bool
+  }
+
+  static defaultProps = {
+    isTrashVisible: false
+  }
+
+  clickTrash = () => {
+    this.props.trashCallback();
+  }
+
   render() {
+    // checking if the isTrashVisible prop is true or false and if true rendering out the img tag
+    let img = ''
+    if (this.props.isTrashVisible) {
+      img = <img src={ TrashIcon } alt="Trash" className="trash" onClick= { this.clickTrash }></img>;
+    }
+
     return (
+      // running the img tag code here
       <div className="bin">
-        <img src={ TrashIcon } alt="Trash" className="trash"></img>
+      {img}
       </div>
     );
   }
